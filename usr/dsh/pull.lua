9e794c3270cd0ea28ad275b6b4216bbf450a6d57
thekaigonzalez <gkai70264@gmail.com>
2021-07-10 02:44:25 -0400

    Update command

---
+++ usr/dsh/pull.lua
@@ -0,0 +1,16 @@
+require "libposix"
+require "libhttp"
+require "libsyscalls"
+local self = {}
+
+function self:Main(a)
+    if a[1] == "+stable" then
+        print("Pulling boot.lua from recent commits..")
+        thread_sleep(2)
+        os.remove("usr/bootloader/bootmanager/boot.lua") -- removes the terminal while keeping the command in memory
+        hdofile("https://raw.githubusercontent.com/thekaigonzalez/Kux/master/bootmgr/default/boot.lua", "usr/bootloader/bootmanager/boot.lua") -- store the update into a file
+        dofile("usr/bootloader/bootmanager/boot.lua")
+    end
+end
+
+return self
