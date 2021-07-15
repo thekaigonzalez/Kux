8c77c2eee62fd5da620684a8de9965c7f19bb92e
kai gonzalez <kai@DESKTOP>
2021-07-15 13:41:50 -0400

    oops

--- usr/dsh/man.lua
+++ usr/dsh/man.lua
@@ -45,12 +45,12 @@ function self:Main(ar)
                 if ar[2] == nil or #ar < 2 then
                     print("no entry found for ''")
                 else
-                    if file_exists("usr/share/man-doc/" .. ar[3] .. ".lua") then
-                        local manpage = require('usr.share.man-doc.' .. ar[3])
+                    if file_exists("usr/share/man-doc/" .. ar[2] .. ".lua") then
+                        local manpage = require('usr.share.man-doc.' .. ar[2])
                         print(ar[2] .. "(" .. manpage.Position .. ") ".. manpage:Series() .. " " .. ar[2] .. "(" .. manpage.Position .. ") ")
                         print("\nNAME\n\t" .. manpage:Name() .. "\nDESCRIPTION\n\t" .. manpage:Description() .. "\nSYNOPSIS\n\t" .. manpage:Synopsis() .. "\nCOPYRIGHT\n\tThis document is licensed under the " .. manpage:Copyright())
                     else
-                        print("no manual entry found for '" .. ar[3] .. "'")
+                        print("no manual entry found for '" .. ar[2] .. "'")
                     end
 
                 end
